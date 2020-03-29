bbb720da07a100d28c875ff26306141911d1c714
Simran Deol <simrandeol@Simrans-MacBook-Pro.local>
2020-03-29 19:14:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default App;
